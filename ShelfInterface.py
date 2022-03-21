f030a0bf034380c1bb4215459e87ee2043c72e33
Entropy <79678575+hdezbriant@users.noreply.github.com>
2022-03-21 10:41:35 -0700

    completed get all children and get all parents. Starting shelves/facings

--- ShelfInterface.py
+++ ShelfInterface.py
@@ -16,9 +16,6 @@ import sys
 
 def shelf_interface(features_geojson):
 
-    # features_geojson = json.dumps(features_geojson['features'], indent = 2)
-    # print(features_geojson)
-
     # Displays menu for user to select desired operation
     menu = """Select one of the following options: 
     1. Retrieve Feature object by ID(if it exists).
@@ -30,36 +27,54 @@ def shelf_interface(features_geojson):
 
     selection = int(input(menu))
 
-
     # Handles user selection to perform chosen task.
     try:
-        if selection == 1:
         # Get Feature by ID
+        if selection == 1:
             id_select = '''Which ID would you like to fetch?\n'''
             feature_id = input(id_select)
             FeatureCollection.get_feature(feature_id)
 
-        elif selection == 2:
         # Get Parent of given Feature
-            # id_select = '''Please enter the ID for the Child Feature object:\n'''
-            # feature_id = input(id_select)
-            # FeatureCollection.get_parent_feature(feature_id)
-            pass
+        elif selection == 2:
+            id_select = '''Please enter the ID for the Child Feature object:\n'''
+            feature_id = input(id_select)
 
-        elif selection == 3:
+            # Finds the requested object and passes into Class method, if it exists
+            child = FeatureCollection.feature_finder(feature_id)
+            if child == None:
+                print("Child not found.")
+                return None
+            elif child:
+                FeatureCollection.get_parent_feature(child)
+            
+
         # Get Children of a Feature
-            print("selected 3")
+        elif selection == 3:
+            id_select = '''Please enter the ID for the Parent Feature object:\n'''
+            feature_id = input(id_select)
 
-        elif selection == 4:
+            # Finds the requested object and passes into Class method, if it exists
+            parent_feature = FeatureCollection.feature_finder(feature_id)
+            if parent_feature == None:
+                print("Parent Object does not exist.")
+                return None
+            elif parent_feature:
+                FeatureCollection.get_children_features(parent_feature)
+
         # Get all Shelves or Facings
-            print("Selected 4")
+        elif selection == 4:
 
-        elif selection == 5:
+            FeatureCollection.get_all_shelves()
+            FeatureCollection.get_all_facings()
+            # Return ALL available Shelf or Facing objs as a List. Else None
+
         # Create compound label for facing
+        elif selection == 5:
             print("Selected 5")
 
-        elif selection == 6:
         # Exit program
+        elif selection == 6:
             print("Exiting...")
             raise SystemExit()
 
@@ -73,24 +88,71 @@ class FeatureCollection:
         self.type = type
         self.features = features
 
-    # Method to retrieve Feature by ID
-    def get_feature(input_feature_id: str) -> None:
+    # Method to find any given feature and return it for further use
+    def feature_finder(input_feature_id: str) -> None:
         for feature in features_geojson['features']:
             if feature['properties']['id'] == input_feature_id:
-                feature = json.dumps(feature, indent = 2)
-                print(feature)
-                break
+                return feature
             elif feature['properties']['id'] != input_feature_id:
                 continue
             else:
                 print(f"ID: {input_feature_id} not found.")
                 return None
+
+    # Method to retrieve Feature by ID and print it
+    def get_feature(feature_id):
+        for feature in features_geojson['features']:
+            if feature['properties']['id'] == feature_id:
+                pretty_feature = json.dumps(feature, indent = 2)
+                print(pretty_feature)
+                print("Feature found!")
+                return feature
+
+            elif feature['properties']['id'] != feature_id:
+                continue
+            else:
+                print(f"ID: {feature_id} not found.")
+                return None
                 
     # given Feature Obj as input, retrieve parent Feature Obj, else None
-    def get_parent_feature(input_feature: object) -> None:
+    def get_parent_feature(input_feature: dict) -> None:
+        childs_parent_id = input_feature['properties']['parent']
+        if childs_parent_id:
+            for feature in features_geojson['features']:
+                if feature['properties']['id'] == childs_parent_id:
+                    feature = json.dumps(feature, indent = 2)
+                    print(feature)
+                    print("Parent found!")
+                    break
+                elif feature['properties']['id'] != childs_parent_id:
+                    continue
+                else:
+                    break
+        else:
+            print(f"This feature does not have a parent.")
+            return None
 
+    # Retrieve children of a chosen object
+    def get_children_features(input_feature: dict) -> list:
+        children_features = []
+        parent_id = input_feature['properties']['id']
+        for feature in features_geojson['features']:
+            if feature['properties']['parent'] == parent_id:
+                children_features.append(feature)
+                continue
+            elif feature['properties']['parent'] != parent_id:
+                continue
+            else:
+                break
+        children_features = json.dumps(children_features, indent = 2)
+        print(children_features)
+        return children_features
+                
+    def get_all_shelves() -> list:
         pass
-
+    
+    def get_all_facings() -> list:
+        pass
 
 class Feature:
     def __init__(self, geometry, ) -> None:
@@ -112,15 +174,6 @@ class properties:
 
 # How to ingest json
 # nad how to retrieve
-
-
-    # get_children_features(input_feature: Feature) -> List[Feature]
-    # given Feature obj as input, retrieve its children Feat Objs as List.
-    # else return []
-
-    # get_all_shelves() -> List[Feature]
-    # get_all_facings() -> List[Feature]
-    # Return ALL available Shelf or Facing objs as a List. Else None
 
     # get_facing_compound_label(facing: Facing) -> str
     # Return formatted string label of parent Shelf label + given Facing label
